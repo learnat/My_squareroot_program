f67ef8fbca09af43d30255543052dbf538cc52a9
bhvay <sreebhavya99033@gmail.com>
2021-09-14 16:23:10 +0530

    changes

--- sqrt.py
+++ sqrt.py
@@ -5,7 +5,5 @@ if hungry=="yes":
     print("i want to eat burger")
     print("i want to eat fries")
 else:
-    thirsty=input("are you thirsty")
-    if thirsty=="yes":
-            print("drink water")
+    print("do your homework")
 
