bf6b8385791b7fd99bed4b8cc6da7c0ae3a586af
bhvay <sreebhavya99033@gmail.com>
2021-09-14 13:44:46 +0530

    my hungry commit

--- sqrt.py
+++ sqrt.py
@@ -2,6 +2,7 @@ num=[1,2,3,4,5,6,7,8,9,10]
 hungry=input("are you hungry")
 if hungry=="yes":
     print(" i want to eat pizza")
+    print("i want to eat burger")
 else:
     print("do your homework")
 
